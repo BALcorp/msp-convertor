e7ed02fa014fa571d0af98559642314f5d92db50
akatz <akatz@henix.fr>
2020-09-26 16:19:18 +0200

    Removed the fluff.

--- lancer_calcul.js
+++
@@ -1,5 +0,0 @@
-var calcul = require("./calcul");
-var res_ttc = calcul.ttc(200,20);
-console.log("ttc="+res_ttc);
-var res_tva = calcul.tva(200,20);
-console.log("tva="+res_tva);
--- lancer_node.bat
+++
@@ -1,8 +0,0 @@
-REM node helloWorld.js
-REM node basic_http_server.js
-REM nodemon basic_http_server.js
-REM node lancer_calcul.js
-REM node first_express_server.js
-REM nodemon server.js
-node server.js
-pause
